d0f172883d35a2ba4db9ce6fc68790d37c90282a
8adam95 <adam.szefer@gmail.com>
2016-09-28 19:32:19 +0200

    first version of the second problem, cannot compile because of updates on the computer

---
+++ Data structures/computerTreeHeight.cpp
@@ -0,0 +1,66 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+#include<queue>
+#include<cstring>
+#include<string>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long 
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+const int MAXN = 100005;
+
+queue<int> Q;
+
+int n, root, height, h[MAXN];
+
+vector<int> adj[MAXN];
+
+void bfs(int u)
+{
+    height++;
+
+    Q.push(u);
+    while(!Q.empty())
+    {
+        int v = Q.front();
+        Q.pop();
+        for(int i = 0; i < adj[v].size(); i++)
+        {
+            h[adj[v][i]] = h[v]+1;
+            Q.push(adj[v][i]);
+        }
+    }
+}
+
+
+int main()
+{
+    scanf("%d", &n);
+    REP(i, n)
+    {
+        int u;
+        scanf("%d", &u);
+        if(u == -1)
+            root = i;
+        else
+            adj[u].PB(i);
+    }
+    
+    h[root] = 0;
+    bfs(root);
+
+    printf("%d\n", height);
+    
+    return 0;
+}
